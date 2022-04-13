5c67ff1ac10ae03cd1d838607088d46ece9ee432
aosothra <arhdam@yandex.ru>
2022-04-13 19:13:16 +0400

    create and setup admin view for Likes

--- property/admin.py
+++ property/admin.py
@@ -8,6 +8,7 @@ class FlatAdmin(admin.ModelAdmin):
     readonly_fields = ('created_at',)
     list_display = ('address', 'price', 'is_building_new', 'construction_year', 'town')
     list_editable = ('is_building_new',)
+    raw_id_fields = ('liked_by',)
     list_filter = ('is_building_new', 'rooms_number', 'has_balcony')
 
 class ComplaintAdmin(admin.ModelAdmin):
