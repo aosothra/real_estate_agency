85f91d40f94ec12e6a1235206f8610c6fdae6789
aosothra <12795376+aosothra@users.noreply.github.com>
2022-04-16 23:26:43 +0400

    register admin models by decorator

--- property/admin.py
+++ property/admin.py
@@ -7,6 +7,7 @@ class OwnersInline(admin.TabularInline):
     model = Flat.owners.through
     raw_id_fields = ('owner',)
 
+@admin.register(Flat)
 class FlatAdmin(admin.ModelAdmin):
     search_fields = ('town', 'address',)
     readonly_fields = ('created_at',)
@@ -16,13 +17,12 @@ class FlatAdmin(admin.ModelAdmin):
     list_filter = ('is_building_new', 'rooms_number', 'has_balcony')
     inlines = (OwnersInline,)
 
+
+@admin.register(Complaint)
 class ComplaintAdmin(admin.ModelAdmin):
     raw_id_fields = ('user', 'flat')
 
+
+@admin.register(Owner)
 class OwnerAdmin(admin.ModelAdmin):
     raw_id_fields = ('flats',)
-
-
-admin.site.register(Flat, FlatAdmin)
-admin.site.register(Complaint, ComplaintAdmin)
-admin.site.register(Owner, OwnerAdmin)
